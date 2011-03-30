c515428fe666928852f2130ad7d6f8aac61740ed
Jared Rodriguez <jrod@blacknode.net>
2011-03-30 12:34:38 -0500

    No direction. Moving over to a real dev system

--- disk_maker.py
+++ disk_maker.py
@@ -7,6 +7,9 @@ class Partition(object):
         self._check_args(fs_type, size)
         self.fs_type = fs_type
         self.size = size
+    
+    def __repr__(self):
+        pass
 
 class FileSystem(object):
     __init__()
@@ -15,13 +18,49 @@ class LVMGroup(object, partition=None):
     pass
 
 class Layout(object):
-    def __init__(self, table='gpt'):
-        self.table = table
+    def __init__(self, size, table='gpt'):
+        self.size = size
         self.partitions = list()
 
     def add_partition(self, partition):
+        pass
+
+    def remove_partition(self, partition):
+        pass
+
+    def show(self):
+        for partition in partitions:
+            print partition
+
+
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        return self.__repr__()
+
+    
+class Size(object):
+    byte = 2**3
+    kilobyte = 2**10
+    megabyte = 2**20
+    gigabyte = 2**30
+    terabyte = 2**40
+    
+    def __init__(self, value):
+        self.bytes = self._analyze(value) 
         
 
+    def _analyze(self, value):
+        valid_suffix = ['k', 'M', 'G', 'T']
+        if type(value) == int:
+            return value
+
+        if type(value) != str:
+            raise SizeObjectValError(
+                    'Value is not in a format I can understand')
+        
+        
 
 class PartitionError(Exception):pass
 
