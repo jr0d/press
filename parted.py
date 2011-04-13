71fcc71a5ef24dba4073dcdca6265e3ef416adcd
Jared Rodriguez <jared.rodriguez@rackspace.com>
2011-04-13 17:53:56 -0500

    Parted interface coming along. It is really dirty, but it has to be done. Have mercy on my soul.

--- parted.py
+++ parted.py
@@ -1,26 +1,71 @@
 import disk_maker
-
+import os
+import shlex
+import subprocess
 from subprocess import Popen, PIPE
 
 class PartedInterface(object):
     '''
     Going to have to work with the parted command line tool due to time
-    constaint. pyparted is not documented and messy. 
+    constaint. pyparted is not documented and messy.
+
+    Once I can use libparted directly, I will move to that.
     '''
     def __init__(self, parted_path='/sbin/parted', device='/dev/sda'):
         self.parted_path = parted_path
         self.device = device
         self.parted = self.parted_path + ' --script ' + self.device
 
-    def print_table(self):
-        print
-        execute(self.parted + ' print')
-                
+    def get_table(self):
+        return robo(self.parted + ' print')
+
+    def get_size(self):
+        table = self.get_table()
+        return table[1].split()[2].strip('B')
+
+    def get_partitions(self):
+        table = self.get_table
+        parititions = list()
+        for row in table:
+            if row[0].isdigit():
+                parititions.append(row.split())
+
+        return partitions
+
+    def remove_partition(self, partition_number):
+        '''
+        Uses run to spawn the process and looks for the return val.
+        '''
+        command = self.parted + ' rm ' + str(partition_number)
+
+        ret = run(command)
+
+        if ret != 0:
+            print 'Could not remove partition: %d' % partition_number
+
+    def wipe_table(self):
+        pass
 
 def execute(command=''):
     p = Popen(command, shell=True)
     status = os.waitpid(p.pid, 0)[1]
     return status
 
-def robo(command=''):
+def robo(command='', output_descriptor='stdout', bufsize=1048576):
+    '''
+    run command and return a list containing each line of output.
+    Change this to:
 
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    '''
+    if output_descriptor == 'stdout' or output_descriptor != 'stderr':
+        pipe = Popen(command, shell=True, bufsize=bufsize, stdout=PIPE).stdout
+    
+    elif output_descriptor == 'stderr':
+        pipe = Popen(command, shell=True, bufsize=bufsize, stderr=PIPE).stderr
+
+    return pipe.read().splitlines()
+
+def run(command=''):
+    return subprocess.call(shlex.split(command), stdout=PIPE, stderr=PIPE)
--- parted_tester.py
+++ parted_tester.py
@@ -1,4 +1,32 @@
-from parted import PartedInterface
+import sys
+from disk_maker import Size
+from parted import *
 
-pi = PartedInterface()
-pi.print_table()
+
+pi = PartedInterface(device='/dev/sdc')
+table_out = pi.get_table()
+
+for line in table_out:
+    print line
+
+
+
+size = pi.get_size()
+print size
+
+s = Size(size)
+
+print s.megabytes()
+
+print 'lets remove a partition!'
+
+print 'Using disk: %s' % pi.device
+choice = raw_input('Would you like to continue? [YES/n]')
+
+if choice != 'YES':
+    print 'WINNING'
+    sys.exit(0)
+
+pi.remove_partition(1)
+
+
