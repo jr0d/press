587ca868547241a1fb7eb4b9b207adb55bed5fc4
Jared Rodriguez <jared.rodriguez@rackspace.com>
2011-10-25 17:07:25 -0500

    Output manager will support curses.

--- press/output.py
+++ press/output.py
@@ -1,4 +1,11 @@
+'''Brainstorming here'''
+
 import sys
 
 class Screen():
+    '''Class defining the state of the screen.'''
+    def __init__(self):
+        print 'Output'
+
+class ScreenElement():
     pass
