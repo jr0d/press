8bbd714d9aed041d4dde3158f83f352153da1cb9
Jared Rodriguez <jared.rodriguez@rackspace.com>
2014-01-29 00:09:25 -0600

    layout add_disk

--- press/structure/layout.py
+++ press/structure/layout.py
@@ -1,8 +1,11 @@
 from press.parted import PartedInterface
+from press.udev import UDevHelper
 from press.structure.size import Size
 
 
 class Disk(object):
+    size = 0
+
     def __init__(self, path, partition_table):
         """
         """
@@ -99,7 +102,6 @@ class VolumeGroup(object):
     Nearly a clone of the PartitionTable container, but for Logical Volumes. There
     are two many naming differences to subclass, however.
 
-    size will be populated
     """
     logical_volumes = list()
 
@@ -180,10 +182,102 @@ class FileSystem(object):
         self.stripe = stripe
 
 
-class Layout(object):
-    def __init__(self):
-        parted_interface = PartedInterface
-
-
 class LayoutValidationError(Exception):
     pass
+
+
+class PhysicalDiskException(Exception):
+    pass
+
+
+class Layout(object):
+    """
+    The highest level class.
+    """
+    disks = list()
+    volume_groups = list()
+
+    def __init__(self, subsystem='all',
+                 use_fibre_channel=True, use_loop_devices=True,
+                 parted_path='/sbin/parted',
+                 default_partition_start=1048576, default_gap=1048576, default_alignment=4096,
+                 disk_association='explicit',
+                 *args, **kwargs):
+        """
+        Docs, maybe later
+
+        disk_association: explicit: path must be associated with Disk object
+                          first: Use only the first available disk, subsequent calls to add_disk
+                            trigger an exception
+                          any: The first available disk is used that can accommodate the
+                            partition table
+        """
+
+        self.fc_enabled = use_fibre_channel
+        self.loop_enabled = use_loop_devices
+        self.parted_path = parted_path
+        self.default_partition_start = default_partition_start
+        self.default_gap = default_gap
+        self.default_alignment = default_alignment
+        self.disk_association = disk_association
+        self.udev = UDevHelper()
+        self.udisks = self.udev.discover_valid_storage_devices(self.fc_enabled, self.loop_enabled)
+        if not self.udisks:
+            raise PhysicalDiskException('The are no valid disks.')
+        self.available_devices = [{
+                                      'DEVNAME': udisk.get('DEVNAME'),
+                                      'DEVLINKS': udisk.get('DEVLINKS'),
+                                      'DEVPATH': udisk.get('DEVPATH')
+                                  }
+                                  for udisk in self.udisks]
+
+    def _find_device_by_ref(self, ref):
+        for idx in xrange(len(self.available_devices)):
+            device  = self.available_devices[idx]
+            if ref ==  device['DEVNAME']:
+                return idx
+            if ref == device.get('DEVPATH'):
+                return idx
+            for link in device.get('DEVLINKS'):
+                if ref == link:
+                    return idx
+        return -1
+
+    def _find_device_by_size(self, size):
+        for idx in xrange(len(self.available_devices)):
+            device = self.available_devices[idx]['DEVNAME']
+            parted = PartedInterface(device, self.parted_path)
+            if size < parted.get_size():
+                return idx
+        return -1
+
+    def add_disk(self, disk):
+        if not isinstance(disk, Disk):
+            raise ValueError('Expected Disk instance.')
+        if not self.available_devices:
+            raise PhysicalDiskException('There are no more available devices.')
+        if self.disk_association == 'explicit':
+            if not disk.path:
+                raise PhysicalDiskException('explicit lookup requires and explicit name.')
+            idx = self._find_device_by_ref(disk.path)
+            if idx == -1:
+                raise PhysicalDiskException('%s is not present.' % disk.path)
+            real_disk = self.available_devices.pop(idx)['DEVNAME']
+        elif self.disk_association == 'first':
+            real_disk = self.available_devices[0]
+            self.available_devices = list()
+        elif self.disk_association == 'any':
+            idx = self._find_device_by_size(disk.partition_table.size)
+            if idx == -1:
+                raise PhysicalDiskException('No disk large enough for partition table.')
+            real_disk = self.available_devices.pop(idx)
+        else:
+            raise ValueError('Unsupported association')
+
+        parted = PartedInterface(real_disk)
+        disk.size = parted.get_size()
+        if disk.size < disk.partition_table.size:
+            raise PhysicalDiskException('Partition table is larger than target disk')
+
+        self.disks.append(disk)
+
