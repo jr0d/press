9b70b653aea5221e0adcc2794f492a6b3cce88dc
Jared Rodriguez <jared.rodriguez@rackspace.com>
2014-06-08 22:14:38 -0500

    adding some examples

---
+++ doc/examples/parted_tester.py
@@ -0,0 +1,43 @@
+from pprint import pprint
+import sys
+
+from press.parted import PartedInterface
+from press.structure import Size
+
+pi = PartedInterface(device='/dev/loop0')
+#if not pi.has_label:
+#    print 'missing label, creating one.'
+#    pi.set_label('msdos')
+#
+
+print pi.get_label()
+
+pprint(pi.device_info)
+
+pprint(pi.partitions)
+
+print pi.wipe_table()
+
+pi.set_label('msdos')
+
+print pi.create_partition('primary', Size('25MiB').bytes, boot_flag=True)
+print pi.create_partition('primary', Size('25MiB').bytes)
+print pi.create_partition('primary', Size('25MiB').bytes)
+print pi.create_partition('logical', Size('25MiB').bytes)
+print pi.create_partition('logical', Size('25MB').bytes)
+
+print pi.partitions
+
+raw_input('press a key')
+
+print pi.wipe_table()
+
+pi.set_label('gpt')
+
+print pi.create_partition('p1', Size('25MiB').bytes, boot_flag=True)
+print pi.create_partition('p2', Size('25MiB').bytes)
+print pi.create_partition('p3', Size('25MiB').bytes)
+print pi.create_partition('p4', Size('25MB').bytes)
+print pi.create_partition('p5', Size('25MiB').bytes)
+
+print pi.partitions
