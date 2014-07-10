682c569cd7fc593634cca5ea07946a60010323fd
Jeffrey Ness <jeffrey.ness@rackspace.com>
2014-07-10 11:40:47 -0500

    Adding file handler

---
+++ press/helpers/file.py
@@ -0,0 +1,53 @@
+
+import os
+
+
+def read(filename):
+    """
+    Reads a file by absolute path and returns it's content.
+
+    :param filename: Absolute path to a file.
+    :type filename: str.
+
+    :return: str.
+
+    """
+    with open(filename, 'r') as f:
+        read_data = f.read()
+    f.close()
+    return read_data
+
+
+def write(filename, data, append=False, mode=0644):
+    """
+    Writes to a file by absolute path.
+
+    :param filename: Absolute path to a file.
+    :type filename: str.
+
+    :param data: A string of data which will be written to file.
+    :type data: str.
+
+    :param append: Should the file be appended to, or over-written.
+    :type append: bool.
+
+    :param mode: The linux permission mode to use for the file.
+    :type mode: int.
+
+    :return: None
+
+    """
+
+    # Use our append keyword argument to
+    # determine our write mode.
+    if append:
+        write_mode = 'w'
+    else:
+        write_mode = 'a'
+
+    with open(filename, write_mode) as f:
+        f.write(data)
+    f.close()
+
+    # Last step lets change the file mode to specified mode
+    os.chmod(filename, mode)
