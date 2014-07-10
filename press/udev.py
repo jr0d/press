c7d7d4f29241896379e2005622c85fa1f8fda09d
Lee <lee@prismnet.com>
2014-07-10 18:11:10 -0500

    Basic fstab creation.

--- press/udev.py
+++ press/udev.py
@@ -30,6 +30,9 @@ class UDevHelper(object):
     def get_device_by_name(self, devname):
         return pyudev.Device.from_device_file(self.context, devname)
 
+    def get_uuid_by_name(self, devname):
+        return pyudev.Device.from_device_file(self.context,devname).get('ID_FS_UUID')
+
     def discover_valid_storage_devices(self, fc_enabled=True, loop_enabled=False):
         """
         Kind of ugly, but gets the job done. It strips devices we don't
