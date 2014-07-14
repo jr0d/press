f151cf05a6426375ce1bfb205b930daca7c0ae25
Jared Rodriguez <jared.rodriguez@rackspace.com>
2014-07-14 17:13:12 -0500

    requests requirement and version bump

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '0.1.2'
+version = '0.1.3'
 
 setup(name='press',
       version=version,
@@ -17,7 +17,8 @@ setup(name='press',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'pyudev'
+          'pyudev',
+          'requests'
       ],
       entry_points="""
       # -*- Entry points: -*-
