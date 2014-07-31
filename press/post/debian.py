e47aa994ac3e9a8c4a398b173acfd89687cdf07f
Jeffrey Ness <jeffrey.ness@rackspace.com>
2014-07-31 09:31:43 -0500

    run func should raise in POST

--- press/post/debian.py
+++ press/post/debian.py
@@ -10,21 +10,14 @@ log = logging.getLogger(__name__)
 
 class DebianPost(Post):
 
-    def apt_get_install(self, packages=[]):
+    def apt_get_install(self, packages):
         """
         Install a package using apt.
         :param packages: A list of strings. Each string is a debian package
         to be installed.
         """
-        if packages:
-            ret = run('apt-get -y install %s' % ' '.join(packages))
-            if not ret.returncode == 0:
-                log.error(
-                    'apt_get_install failed with return_code: %s. Reasons: %s'
-                    % ret.returncode, ret.stderr
-                )
-                raise PostException(ret.stderr)
-            return ret
+        return run('apt-get -y install %s' % ' '.join(packages),
+                   raise_exception=True)
 
     def install_packages(self, packages=[]):
         """
@@ -41,5 +34,5 @@ class DebianPost(Post):
 
         :param disk: Disk as a string /dev/sda
         """
-        run('grub-mkconfig -o /boot/grub/grub.cfg')
-        run('grub-install %s' % disk)
+        run('grub-mkconfig -o /boot/grub/grub.cfg', raise_exception=True)
+        run('grub-install %s' % disk, raise_exception=True)
