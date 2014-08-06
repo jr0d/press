fc533877823667313f077346afce164ae159edc4
Jeffrey Ness <jeffrey.ness@rackspace.com>
2014-08-06 14:06:14 -0500

    not in chroot yet, fix path

--- doc/examples/end_to_end_test.py
+++ doc/examples/end_to_end_test.py
@@ -62,7 +62,7 @@ dl.extract(new_root)
 
 # Nessy adding some POST action
 run('cp /etc/resolv.conf %s/etc/resolv.conf' % new_root)
-run('mv /etc/fstab_rs %s/etc/fstab' % new_root)
+run('mv %s/etc/fstab_rs %s/etc/fstab' % (new_root, new_root))
 
 post = DebianPost(new_root)
 post.useradd('rack')
