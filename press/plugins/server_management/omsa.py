5e390f72c25e92ac170d297ed5d8b2afdf159b37
Chris Griffith <chris.griffith@rackspace.com>
2015-10-08 15:15:49 -0500

    Adding OMSA for RedHat Class

--- press/plugins/server_management/omsa.py
+++ press/plugins/server_management/omsa.py
@@ -7,6 +7,7 @@ pgp_key_file = 'dell_key.1285491434D8786F'
 
 log = logging.getLogger('press.plugins.server_management')
 
+
 class OMSADebian(TargetExtension):
     dist = ''
     mirrorbase = 'http://mirror.rackspace.com/dell/community/ubuntu'
@@ -45,5 +46,44 @@ class OMSAUbuntu1404(OMSADebian):
     __extends__ = 'ubuntu_1404'
     dist = 'trusty'
 
+
+class OMSARedHat(TargetExtension):
+
+    def __init__(self, target_obj, version='7'):
+        self.version = version
+        self.repository_rpm_url = 'http://mirror.rackspace.com/dell/hardware/latest/mirrors.cgi/' \
+                                  'osname=rhel{{version}}&basearch=x86_64' \
+                                  '&native=1&getrpm=dell-omsa-repository&redirpath='.format(version=self.version)
+        super(OMSARedHat, self).__init__(target_obj)
+
+    def download_and_prepare_repositories(self):
+        self.target.chroot('try wget -O dell-omsa-repository.rpm "{0}"'.format(self.repository_rpm_url))
+        self.target.chroot('echo bootstrapurl=http://mirror.rackspace.com/dell/hardware/latest/bootstrap.cgi'
+                           '  > /etc/yum.repos.d/dell-omsa-repository.repo')
+
+    def install_omsa_repo(self):
+        self.target.chroot('rpm -i dell-omsa-repository.rpm')
+
+    def install_openmanage(self):
+        self.target.install_package('srvadmin-all')
+
+    def run(self):
+        self.download_and_prepare_repositories()
+        self.install_omsa_repo()
+        self.install_openmanage()
+
+
+class OMSARHEL7(OMSARedHat):
+    __extends__ = 'rhel_7'
+
+    def __init__(self, target_obj):
+        super(OMSARHEL7, self).__init__(target_obj, version='7')
+
+
+class OMSACentOS7(OMSARedHat):
+    __extends__ = 'centos_7'
+
+    def __init__(self, target_obj):
+        super(OMSACentOS7, self).__init__(target_obj, version='7')
 
 
