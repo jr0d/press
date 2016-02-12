694847b6d80f37e048c1c8d72569197ec05269c8
Lee <lee@prismnet.com>
2016-02-12 16:24:04 -0600

    Updating targets to import EL6

--- press/targets/__init__.py
+++ press/targets/__init__.py
@@ -13,3 +13,4 @@ import linux.debian.ubuntu_1404.ubuntu1404_target
 import linux.redhat.redhat_target
 import linux.redhat.enterprise_linux.enterprise_linux_target
 import linux.redhat.enterprise_linux.enterprise_linux_7.enterprise_linux_7_target
+import linux.redhat.enterprise_linux.enterprise_linux_6.enterprise_linux_6_target
