3fd8c5430c6ac0e59ac12a083a3fedd7abc97ab1
Jared Rodriguez <jared.rodriguez@rackspace.com>
2018-05-07 12:24:45 -0500

    fix logging

--- press/log.py
+++ press/log.py
@@ -7,7 +7,6 @@ def setup_logging(log_level=logging.ERROR,
                   console_logging=True,
                   log_file=None,
                   cli_debug=False):
-    logging.basicConfig(format=FORMAT)
 
     press_logger = logging.getLogger('press')
     press_cli_logger = logging.getLogger('press.helpers.cli')
@@ -18,8 +17,10 @@ def setup_logging(log_level=logging.ERROR,
         press_logger.setLevel(log_level)
 
     if log_file:
+        fh = logging.FileHandler(log_file)
+        fh.setFormatter(logging.Formatter(fmt=FORMAT))
         press_logger.info('Setting log file: {}'.format(log_file))
-        press_logger.addHandler(logging.FileHandler(log_file))
+        press_logger.addHandler(fh)
 
     if not cli_debug:
         press_cli_logger.setLevel(logging.ERROR)
