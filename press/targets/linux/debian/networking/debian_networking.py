109ed59ab63dab4970462b7ec12bba0d6881d139
Scott Tran <Scott.Tran@rackspace.com>
2018-06-13 14:29:05 -0500

    DCXOSLINUX-4669, Configure network interfaces using netplan for Ubuntu 18.04 (#205)
    * DCXOSLINUX-4669, Configure network interfaces using netplan for Ubuntu 1804
    
    * DCXOSLINUX-4669, Configure network interfaces using netplan for Ubuntu 18.04

--- press/targets/linux/debian/networking/debian_networking.py
+++ press/targets/linux/debian/networking/debian_networking.py
@@ -10,13 +10,18 @@ from jinja2 import Environment
 log = logging.getLogger(__name__)
 
 
-def get_template_path():
-    this_dir = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(this_dir, 'interfaces.template')
+def render_template(template_path, networks):
+    """
+    Render a network configuration file using Jinja templating.
 
+    Args:
+        template_path (str): The path to a file that contains Jinja templating.
+        networks (dict): A dictionary containing network information.
 
-def render_template(networks):
-    template_path = get_template_path()
+    Returns:
+        str: A rendered unicode string derived from the Jinja template and the
+            network configuration.
+    """
     if not os.path.exists(template_path):
         log.warn('Interfaces template is missing')
         return
@@ -29,7 +34,16 @@ def render_template(networks):
     return data
 
 
-def write_interfaces(path, network_configuration):
+def write_interfaces(path, network_configuration, os_name=None):
+    """
+    Write a network interface configuration file to configure network on the target.
+
+    Args:
+        path (str): The network configuration file that will be created for configuring network.
+        network_configuration (dict): A dictionary containing network configuration.
+        os_name (str): A string representing the OS name of the target. The OS name will determine the
+            the type of configuration file to write (interfaces vs netplan). (optional)
+    """
     interfaces = network_configuration.get('interfaces', list())
     networks = network_configuration.get('networks')
 
@@ -38,8 +52,18 @@ def write_interfaces(path, network_configuration):
                                                         interface.get(
                                                             'missing_ok', False))
         for network in networks:
+            if os_name == 'ubuntu_1804' and 'netmask' in network and 'prefix' not in network:
+                # convert netmask to a 'prefix' value used by netplan_interface.template
+                network['prefix'] = sum([bin(int(x)).count('1')
+                                        for x in network['netmask'].split('.')])
             if name == network.get('interface'):
                 network['device'] = device.devname
                 network['type'] = network.get('type', 'AF_INET')
 
-    deployment.write(path, render_template(networks))
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    if os_name == 'ubuntu_1804':
+        template = os.path.join(this_dir, 'netplan_interface.template')
+        deployment.write(path, render_template(template, network_configuration))
+    else:
+        template = os.path.join(this_dir, 'interface.template')
+        deployment.write(path, render_template(template, networks))
--- press/targets/linux/debian/ubuntu_1804/ubuntu_1804_target.py
+++ press/targets/linux/debian/ubuntu_1804/ubuntu_1804_target.py
@@ -11,14 +11,17 @@ log = logging.getLogger(__name__)
 class Ubuntu1804Target(DebianTarget, Grub2Debian):
     name = 'ubuntu_1804'
     dist = 'bionic'
+    interfaces_path = '/etc/netplan/01-netcfg.yaml'
 
     def check_for_grub(self):
+        """Check for grub multiboot boot loader and install its required packages if needed."""
         if sysfs_info.has_efi():
             _required_packages = ['shim-signed', 'grub-efi-amd64-signed']
             if self.install_packages(_required_packages):
                 raise GeneralPostTargetError('Error installing required packages for grub2')
 
     def run(self):
+        """Run Debian functions."""
         super(Ubuntu1804Target, self).run()
         self.grub_disable_recovery()
         self.check_for_grub()
