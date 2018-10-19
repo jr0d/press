2c6e73270944fc40c13600ba912d441d3cc4f458
Jared Rodriguez <jared.rodriguez@rackspace.com>
2018-10-18 23:57:26 -0500

    base ubuntu_1804

--- press/targets/__init__.py
+++ press/targets/__init__.py
@@ -7,6 +7,7 @@ from press.targets.linux import linux_target
 from press.targets.linux.debian import debian_target
 from press.targets.linux.debian.ubuntu_1404 import ubuntu1404_target
 from press.targets.linux.debian.ubuntu_1604 import ubuntu_1604_target
+from press.targets.linux.debian.ubuntu_1804 import ubuntu_1804_target
 from press.targets.linux.redhat import redhat_target
 from press.targets.linux.redhat.enterprise_linux import enterprise_linux_target
 from press.targets.linux.redhat.enterprise_linux.enterprise_linux_6 import enterprise_linux_6_target
