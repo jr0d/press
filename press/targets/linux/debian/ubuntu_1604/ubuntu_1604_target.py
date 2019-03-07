280d0ad40ea7f309fde46cdf5ef49056ed3dd846
Lee Scott <lee.scott@rackspace.com>
2019-03-07 09:41:52 -0600

    DCXOSLINUX-4883 install HWE kernel for 1604+R740 (#224)

--- press/targets/linux/debian/ubuntu_1604/ubuntu_1604_target.py
+++ press/targets/linux/debian/ubuntu_1604/ubuntu_1604_target.py
@@ -1,6 +1,6 @@
 import logging
 
-from press.helpers import deployment, sysfs_info
+from press.helpers import sysfs_info
 from press.targets import GeneralPostTargetError
 from press.targets.linux.grub2_debian_target import Grub2Debian
 from press.targets.linux.debian.debian_target import DebianTarget
@@ -19,8 +19,16 @@ class Ubuntu1604Target(DebianTarget, Grub2Debian):
                 raise GeneralPostTargetError(
                     'Error installing required packages for grub2')
 
+    def install_hwe_kernel_on_14gen(self):
+        """16.04 needs HWE kernel for R740"""
+        if 'R740' in self.get_product_name():
+            _hwe_kernel = ['linux-generic-hwe-16.04']
+            if self.install_packages(_hwe_kernel):
+                raise GeneralPostTargetError('Error installing HWE kernel for R740')
+
     def run(self):
         super(Ubuntu1604Target, self).run()
+        self.install_hwe_kernel_on_14gen()
         self.grub_disable_recovery()
         self.check_for_grub()
         self.install_grub2()
